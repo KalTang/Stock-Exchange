bc08e1dcd7f6f3ee712cb0441265a7dbb8e37fbe
Kalvin Tang <ktang82@my.bcit.ca>
2021-03-30 15:37:50 -0700

    #5 fixed imports

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, SafeAreaView, Button } from 'react-native';
 
 const Register = () => {
     return (
