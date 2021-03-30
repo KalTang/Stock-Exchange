68f2ea5686c7f7a602893e6a7def3ecb77d782ff
Kalvin Tang <ktang82@my.bcit.ca>
2021-03-30 12:02:54 -0700

    #5 Register page creation

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -3,9 +3,30 @@ import { View, Text } from 'react-native';
 
 const Register = () => {
     return (
-        <View>
-            <Text>Register</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Register</Text>
+
+            <TextInput style={styles.formInput} placeholder="First Name" />
+            <TextInput style={styles.formInput} placeholder="Last Name" />
+            <TextInput style={styles.formInput} placeholder="Email" />
+            <TextInput
+                style={styles.formInput}
+                placeholder="Password"
+                secureTextEntry={true}
+            />
+            <TextInput
+                style={styles.formInput}
+                placeholder="Confirm password"
+                secureTextEntry={true}
+            />
+
+            <Button title="Sign Up" />
+
+            <Text style={{ color: '#b73535', fontSize: 18, marginTop: 20 }}>
+                Already have an account?
+            </Text>
+            <Button title="Login here" />
+        </SafeAreaView>
     );
 };
 
