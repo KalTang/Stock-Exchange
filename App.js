b3b4a4798f524113dd7a9fd877b1a135d804a0b9
Kalvin Tang <ktang82@my.bcit.ca>
2021-03-30 09:06:32 -0700

    set titlepage with button layout #4

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>I am working! </Text>
+            <Text style={styles.title}>Welcome to</Text>
+            <Text style={styles.title}>The Stock Exchange App</Text>
             <StatusBar style="auto" />
+
+            <Button title="Login" />
+            <Button title="Register" />
         </View>
     );
 }
@@ -14,8 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+
+        backgroundColor: '#121212',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#b73535',
     },
 });
