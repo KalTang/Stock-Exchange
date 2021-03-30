a537fec1310ef86b1d006ee65aaafd1766fa9d76
Kalvin Tang <ktang82@my.bcit.ca>
2021-03-30 15:53:31 -0700

    #3 login page layout + bit of styling

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    SafeAreaView,
+    Button,
+    StyleSheet,
+} from 'react-native';
 
 const Login = () => {
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput style={styles.formInput} placeholder="Email" />
+            <TextInput
+                style={styles.formInput}
+                placeholder="Password"
+                secureTextEntry={true}
+            />
+
+            <Button title="Login" />
+
+            <Text style={{ color: '#b73535', fontSize: 18, marginTop: 20 }}>
+                Don't have an account?
+            </Text>
+            <Button title="Sign up here" />
+        </SafeAreaView>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+        backgroundColor: '#121212',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: '#b73535',
+        marginBottom: 15,
+    },
+    formInput: {
+        fontSize: 20,
+        color: '#b73535',
+        marginBottom: 15,
+    },
+});
 
 export default Login;
